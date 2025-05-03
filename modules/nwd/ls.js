64450c94651b952383913d52a81445e8308029eb
lg0leg <lg.oleg1067@gmail.com>
2025-05-03 22:07:59 +0300

    feat: improve "ls" function

--- modules/nwd/ls.js
+++ modules/nwd/ls.js
@@ -3,9 +3,23 @@ import { cwd } from 'process';
 
 export const ls = async () => {
   try {
-    const files = await readdir(cwd());
+    const allFiles = await readdir(cwd(), { withFileTypes: true });
+    const files = [];
+    const folders = [];
 
-    console.table(files);
+    allFiles.forEach((item) => {
+      if (item.isDirectory()) {
+        folders.push({ Name: item.name, Type: 'directory' });
+      } else {
+        files.push({ Name: item.name, Type: 'file' });
+      }
+    });
+
+    const sortFiles = files.sort((a, b) => a.Name.localeCompare(b.Name));
+    const sortFolders = folders.sort((a, b) => a.Name.localeCompare(b.Name));
+    const res = [...sortFolders, ...sortFiles];
+
+    console.table(res);
   } catch (err) {
     throw new Error('Operation failed');
   }
